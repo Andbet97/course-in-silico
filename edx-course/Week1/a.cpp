98127367dd6317f806776bca87ae45ec742817df
utp <utp@utp>
2017-03-09 10:58:52 -0500

    ejercicio 1 del curso

---
+++ edx-course/Week1/a.cpp
@@ -0,0 +1,19 @@
+#ifdef JUDGE
+#include <fstream>
+std::ifstream cin("input.txt");
+std::ofstream cout("output.txt");
+#else
+#include <iostream>
+using std::cin;
+using std::cout;
+#endif
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
