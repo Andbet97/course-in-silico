91e8ff9aa3fa589670054891cf9a5b101ce6f6cc
Andbet <andbet050197@gmail.com>
2017-03-09 19:53:29 -0500

    Ejercicio Hacker Rank

---
+++ hacker-rank/solvermefirst.c
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> //libreria que incluye todas las librerias estandar
+using namespace std;
+
+int main ()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    return 0;
+}
