cf14d666e100e42e8c0fc486bcfb4db6e9dd93fb
utp <utp@utp>
2017-03-28 10:52:24 -0500

    manejo de entradas en los jueces

---
+++ ejemplos-charlas/ejemploentradas.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+int main ()
+{
+  /*int N;
+  cin >> N;
+  while(N--) {
+    int r;
+    cin >> r;
+    for (int i = 0; i < r; i++) {
+      int x;
+      cin >> x;
+    }
+    count << endl;
+  }*/
+
+  /*int tc;
+  while (cin >> tc and tc != 0){
+    //codigo}
+    for (int i = 0; i < tc; i++) {
+      int x;
+      cin >> x;
+    }
+    cout << endl;
+  }*/
+
+  /*int tc;
+  while (cin >> tc){
+    //codigo}
+    for (int i = 0; i < tc; i++) {
+      int x;
+      cin >> x;
+    }
+    cout << endl;
+  }*/
+
+  /*int tc;
+  cin >> tc;
+  while (tc--) {
+    string input;
+    cin >> input;
+  }*/
+
+  /*int tc;
+  cin >> tc;
+  while (tc--) {
+    string input;
+    getline(cin, input);
+  }*/
+
+  while (getline(cin, input)) {
+    cout << input;
+  }
+
+  
+  return 0;
+}
